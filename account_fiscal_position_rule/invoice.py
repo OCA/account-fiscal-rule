9841a9a8b24bf4a8be6027a1b19ea49ed8a69d0b
Renato Lima <renato@renato-laptop>
2010-07-07 13:52:52 -0300

    changed fiscal_classification_id_change method in product.product and added company_id.id parameter in search method in account.tax objects

--- account_fiscal_position_rule/invoice.py
+++ account_fiscal_position_rule/invoice.py
@@ -52,10 +52,11 @@ class account_invoice(osv.osv):
         to_country = partner_addr_default.country_id.id
         to_state = partner_addr_default.state_id.id
 
-        fsc_pos_id = self.pool.get('account.fiscal.position.rule').search(cr, uid, [('from_country','=',from_country),('from_state','=',from_state),('to_country','=',to_country),('to_state','=',to_state),('use_invoice','=',True)])
+        fsc_pos_id = self.pool.get('account.fiscal.position.rule').search(cr, uid, [('company_id','=',company_id), ('from_country','=',from_country),('from_state','=',from_state),('to_country','=',to_country),('to_state','=',to_state),('use_invoice','=',True)])
         
         if fsc_pos_id:
-            result['value']['fiscal_position'] = fsc_pos_id[0]
+            obj_fpo_rule = self.pool.get('account.fiscal.position.rule').browse(cr, uid, fsc_pos_id)[0]
+            result['value']['fiscal_position'] = obj_fpo_rule.fiscal_position_id.id
 
         return result
     
@@ -63,7 +64,7 @@ class account_invoice(osv.osv):
          
         result = {'value': {'fiscal_position': False}}
         
-        if not ptn_id or not cpy_id:
+        if not ptn_id or not cpy_id or not ptn_invoice_id:
             return result
         
         if result['value']['fiscal_position']:
@@ -82,10 +83,43 @@ class account_invoice(osv.osv):
         to_country = partner_addr_invoice.country_id.id
         to_state = partner_addr_invoice.state_id.id
 
-        fsc_pos_id = self.pool.get('account.fiscal.position.rule').search(cr, uid, [('from_country','=',from_country),('from_state','=',from_state),('to_country','=',to_country),('to_state','=',to_state),('use_invoice','=',True)])
-        if fsc_pos_id: 
-            result['value']['fiscal_position'] = fsc_pos_id[0]
+        fsc_pos_id = self.pool.get('account.fiscal.position.rule').search(cr, uid, [('company_id','=', cpy_id), ('from_country','=',from_country),('from_state','=',from_state),('to_country','=',to_country),('to_state','=',to_state),('use_invoice','=',True)])
+        
+        if fsc_pos_id:
+            obj_fpo_rule = self.pool.get('account.fiscal.position.rule').browse(cr, uid, fsc_pos_id)[0]
+            result['value']['fiscal_position'] = obj_fpo_rule.fiscal_position_id.id
        
-        return result        
+        return result   
+    
+    def onchange_address_invoice_id(self, cr, uid, ids, cpy_id, ptn_id, ptn_invoice_id):
+
+        result = {'value': {'fiscal_position': False}}
+        
+        if not ptn_id or not cpy_id or not ptn_invoice_id:
+            return result
+        
+        if result['value']['fiscal_position']:
+            return result
+        
+        obj_company = self.pool.get('res.company').browse(cr, uid, [cpy_id])[0]
+        
+        company_addr = self.pool.get('res.partner').address_get(cr, uid, [obj_company.partner_id.id], ['default'])
+        company_addr_default = self.pool.get('res.partner.address').browse(cr, uid, [company_addr['default']])[0]
+        
+        from_country = company_addr_default.country_id.id
+        from_state = company_addr_default.state_id.id
+
+        partner_addr_invoice = self.pool.get('res.partner.address').browse(cr, uid, [ptn_invoice_id])[0]
+
+        to_country = partner_addr_invoice.country_id.id
+        to_state = partner_addr_invoice.state_id.id
+
+        fsc_pos_id = self.pool.get('account.fiscal.position.rule').search(cr, uid, [('company_id','=', cpy_id), ('from_country','=',from_country),('from_state','=',from_state),('to_country','=',to_country),('to_state','=',to_state),('use_invoice','=',True)])
+        
+        if fsc_pos_id:
+            obj_fpo_rule = self.pool.get('account.fiscal.position.rule').browse(cr, uid, fsc_pos_id)[0]
+            result['value']['fiscal_position'] = obj_fpo_rule.fiscal_position_id.id
+       
+        return result      
         
 account_invoice()
