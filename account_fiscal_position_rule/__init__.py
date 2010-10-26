7415234ae8880d038f3117dc141c339f6987ed35
Renato Lima <renato@renato-laptop>
2010-10-26 04:11:54 -0200

    Refactored account_fiscal_position_rule and created account_fiscal_position_rule_sale for sales, account_fiscal_position_rule_stock for stock and account_fiscal_position_rule_purchase for purchase

--- account_fiscal_position_rule/__init__.py
+++ account_fiscal_position_rule/__init__.py
@@ -21,8 +21,6 @@
 #############################################################################
 
 import account_fiscal_position_rule
-import sale
 import invoice
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
