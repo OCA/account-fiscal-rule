eea98b381f19351b84829d3d7ec807358e493100
unknown <renatonlima@gmail.com>
2011-11-11 03:35:18 -0200

    account_product_fiscal_classification, add Update All Produtos Button in Fiscal Classification

--- account_product_fiscal_classification/account_product_fiscal_classification.py
+++ account_product_fiscal_classification/account_product_fiscal_classification.py
@@ -1,21 +1,21 @@
 # -*- encoding: utf-8 -*-
-#################################################################################
-#                                                                               #
-# Copyright (C) 2009  Renato Lima - Akretion                                    #
-#                                                                               #
-#This program is free software: you can redistribute it and/or modify           #
-#it under the terms of the GNU Affero General Public License as published by    #
-#the Free Software Foundation, either version 3 of the License, or              #
-#(at your option) any later version.                                            #
-#                                                                               #
-#This program is distributed in the hope that it will be useful,                #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-#GNU General Public License for more details.                                   #
-#                                                                               #
-#You should have received a copy of the GNU General Public License              #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
-#################################################################################
+################################################################################
+#                                                                              #
+# Copyright (C) 2010  Raphaël Valyi, Renato Lima - Akretion                    #
+#                                                                              #
+#This program is free software: you can redistribute it and/or modify          #
+#it under the terms of the GNU Affero General Public License as published by   #
+#the Free Software Foundation, either version 3 of the License, or             #
+#(at your option) any later version.                                           #
+#                                                                              #
+#This program is distributed in the hope that it will be useful,               #
+#but WITHOUT ANY WARRANTY; without even the implied warranty of                #
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 #
+#GNU General Public License for more details.                                  #
+#                                                                              #
+#You should have received a copy of the GNU General Public License             #
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.         #
+################################################################################
 
 from osv import fields, osv
 
@@ -30,7 +30,35 @@ class account_product_fiscal_classification(osv.osv):
         'sale_base_tax_ids': fields.many2many('account.tax', 'fiscal_classification_sale_tax_rel', 'fiscal_classification_id', 'tax_id', 'Base Sale Taxes', domain=[('type_tax_use','!=','purchase')]),
         'purchase_base_tax_ids': fields.many2many('account.tax', 'fiscal_classification_purchase_tax_rel', 'fiscal_classification_id', 'tax_id', 'Base Purchase Taxes', domain=[('type_tax_use','!=','sale')]),
     }
+    
+    def button_update_products(self, cr, uid, ids, context=None):
+        obj_product = self.pool.get('product.product')
+        obj_company = self.pool.get('res.company')
 
+        result = True        
+        if not context:
+            context = {}
+        
+        for fiscal_classification in self.browse(cr, uid, ids):
+            current_company_id = self.pool.get('res.users').browse(cr, uid, uid).company_id.id
+            
+            product_ids = obj_product.search(cr, uid, [])
+            product_read = obj_product.read(cr, uid, product_ids, ['property_fiscal_classification'])
+            product_ids_new = [x['id'] for x in product_read if x['property_fiscal_classification'] and x['property_fiscal_classification'][0] == fiscal_classification.id]
+            
+            for product in obj_product.browse(cr, uid, product_ids_new, context):
+                to_keep_sale_tax_ids = self.pool.get('account.tax').search(cr, uid, [('id', 'in', [x.id for x in product.taxes_id]), ('company_id', '!=', current_company_id)])
+                to_keep_purchase_tax_ids = self.pool.get('account.tax').search(cr, uid, [('id', 'in',[x.id for x in product.supplier_taxes_id]), ('company_id', '!=', current_company_id)])
+            
+                vals = {
+                        'taxes_id': [(6, 0, to_keep_sale_tax_ids + [x.id for x in fiscal_classification.sale_base_tax_ids])],
+                        'supplier_taxes_id': [(6, 0, to_keep_purchase_tax_ids + [x.id for x in fiscal_classification.purchase_base_tax_ids])],
+                        }
+            
+                obj_product.write(cr, uid, product.id, vals, context)
+        
+        return result
+    
     def name_search(self, cr, user, name='', args=None, operator='ilike', context=None, limit=80):
         if not args:
             args = []
