b37a09897e1633ade13c9149cd9c08fe7c1bd19e
unknown <renatonlima@gmail.com>
2011-12-21 15:40:13 -0200

    account_fiscal_position_rule: Added Start and End dates in fiscal position rules to determine the rule by date range

--- account_fiscal_position_rule_stock/stock.py
+++ account_fiscal_position_rule_stock/stock.py
@@ -11,15 +11,16 @@
 #This program is distributed in the hope that it will be useful,                #
 #but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
 #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
-#GNU General Public License for more details.                                   #
+#GNU Affero General Public License for more details.                            #
 #                                                                               #
-#You should have received a copy of the GNU General Public License              #
+#You should have received a copy of the GNU Affero General Public License       #
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.          #
 #################################################################################
 
 from osv import osv, fields
 
 class stock_picking(osv.osv):
+    
     _inherit = "stock.picking"
     _description = "Picking List"
 
@@ -48,3 +49,5 @@ class stock_picking(osv.osv):
 
 
 stock_picking()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
