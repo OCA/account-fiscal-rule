901128007ea1d0406ba39c28780363b5a2db5f34
unknown <renatonlima@gmail.com>
2011-12-21 15:43:22 -0200

    changed file invoice.py to account_invoice.py in module account_fiscal_position_rule

--- account_fiscal_position_rule/__init__.py
+++ account_fiscal_position_rule/__init__.py
@@ -18,6 +18,6 @@
 #################################################################################
 
 import account_fiscal_position_rule
-import invoice
+import account_invoice
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
