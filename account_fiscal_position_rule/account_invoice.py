26ceede636f2a57068df1da7ebfd016daf22bf98
unknown <renatonlima@gmail.com>
2012-04-02 18:05:50 -0300

    account_fiscal_position_rule regression, last modification made by camp2camp broked brazilian localization, I suggest if not possible use account fiscal position rule models and wizard to create all rules by companies and countries or create a new branch or new modules in addons extra to implement these modifications.

--- account_fiscal_position_rule/account_invoice.py
+++ account_fiscal_position_rule/account_invoice.py
@@ -19,55 +19,54 @@
 
 from osv import fields, osv
 
-
 class account_invoice(osv.osv):
-
+    
     _inherit = 'account.invoice'
 
     def onchange_partner_id(self, cr, uid, ids, type, partner_id,\
             date_invoice=False, payment_term=False, partner_bank_id=False, company_id=False):
 
         result = super(account_invoice, self).onchange_partner_id(cr, uid, ids, type, partner_id, date_invoice, payment_term, partner_bank_id, company_id)
-
+        
         if not partner_id or not company_id or not result['value']['address_invoice_id']:
             return result
 
         partner_invoice_id = result['value'].get('partner_invoice_id', False)
         obj_fiscal_position_rule = self.pool.get('account.fiscal.position.rule')
-        fiscal_result = obj_fiscal_position_rule.fiscal_position_map(cr, uid,  partner_id, partner_invoice_id, company_id, context={'use_domain': ('use_invoice', '=', True)})
-
+        fiscal_result = obj_fiscal_position_rule.fiscal_position_map(cr, uid,  partner_id, partner_invoice_id, company_id, context={'use_domain': ('use_invoice','=',True)})
+        
         result['value'].update(fiscal_result)
 
         return result
-
+                            
     def onchange_company_id(self, cr, uid, ids, company_id, part_id, type, invoice_line, currency_id, ptn_invoice_id):
-
+         
         result = super(account_invoice, self).onchange_company_id(cr, uid, ids, company_id, part_id, type, invoice_line, currency_id)
-
+        
         if not part_id or not company_id or not ptn_invoice_id:
             return result
-
+        
         obj_fiscal_position_rule = self.pool.get('account.fiscal.position.rule')
-        fiscal_result = obj_fiscal_position_rule.fiscal_position_map(cr, uid,  part_id, ptn_invoice_id, company_id, context={'use_domain': ('use_invoice', '=', True)})
-
+        fiscal_result = obj_fiscal_position_rule.fiscal_position_map(cr, uid,  part_id, ptn_invoice_id, company_id, context={'use_domain': ('use_invoice','=',True)})
+        
         result['value'].update(fiscal_result)
 
         return result
-
+    
     def onchange_address_invoice_id(self, cr, uid, ids, cpy_id, ptn_id, ptn_invoice_id):
 
         result = {'value': {'fiscal_position': False}}
-
+        
         if not ptn_id or not cpy_id or not ptn_invoice_id:
             return result
 
         obj_fiscal_position_rule = self.pool.get('account.fiscal.position.rule')
-        fiscal_result = obj_fiscal_position_rule.fiscal_position_map(cr, uid,  ptn_id, ptn_invoice_id, cpy_id, context={'use_domain': ('use_invoice', '=', True)})
-
+        fiscal_result = obj_fiscal_position_rule.fiscal_position_map(cr, uid,  ptn_id, ptn_invoice_id, cpy_id, context={'use_domain': ('use_invoice','=',True)})
+        
         result['value'].update(fiscal_result)
-
+        
         return result
-
+        
 account_invoice()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
