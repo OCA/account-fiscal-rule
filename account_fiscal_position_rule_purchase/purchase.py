46eb0c390da7b0ca652866828e4fc9d58e6893f9
sebastien beau <sebastien.beau@akretion.com.br>
2012-10-26 16:52:39 +0200

    [REF] Clean syntax

--- account_fiscal_position_rule_purchase/purchase.py
+++ account_fiscal_position_rule_purchase/purchase.py
@@ -22,20 +22,20 @@ from osv import fields, osv
 class purchase_order(osv.osv):
 
     _inherit = 'purchase.order'
-    
+
     def onchange_partner_id(self, cr, uid, ids, part, company_id=False):
-        
+
         result = super(purchase_order, self ).onchange_partner_id(cr, uid, ids, part)
-        
+
         if not part or not company_id or not result['value']['partner_address_id']:
             return result
 
         partner_address_id = result['value'].get('partner_address_id', False)
         obj_fiscal_position_rule = self.pool.get('account.fiscal.position.rule')
         fiscal_result = obj_fiscal_position_rule.fiscal_position_map(cr, uid,  part, partner_address_id, company_id, context={'use_domain': ('use_purchase','=',True)})
-        
+
         result['value'].update(fiscal_result)
-        
+
         return result
 
     def onchange_partner_address_id(self, cr, uid, ids, partner_address_id, company_id=False):
@@ -48,11 +48,11 @@ class purchase_order(osv.osv):
         partner_addr = self.pool.get('res.partner.address').browse(cr, uid, partner_address_id)
         obj_fiscal_position_rule = self.pool.get('account.fiscal.position.rule')
         fiscal_result = obj_fiscal_position_rule.fiscal_position_map(cr, uid,  partner_addr.partner_id.id, partner_address_id, company_id, context={'use_domain': ('use_purchase','=',True)})
-        
+
         result['value'].update(fiscal_result)
-        
+
         return result
-    
+
     def onchange_company_id(self, cr, uid, ids, partner_id, partner_address_id=False, company_id=False):
 
         result = {'value': {'fiscal_position': False}}
@@ -62,9 +62,9 @@ class purchase_order(osv.osv):
 
         obj_fiscal_position_rule = self.pool.get('account.fiscal.position.rule')
         fiscal_result = obj_fiscal_position_rule.fiscal_position_map(cr, uid,  partner_id, partner_address_id, company_id, context={'use_domain': ('use_purchase','=',True)})
-        
+
         result['value'].update(fiscal_result)
-        
+
         return result
 
 purchase_order()
