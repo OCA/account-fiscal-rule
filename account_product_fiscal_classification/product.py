e38646a19c371f9e428f7c7637cee7079726dacd
sebastien beau <sebastien.beau@akretion.com.br>
2012-10-29 14:41:30 +0100

    [REF] Update copyright

--- account_product_fiscal_classification/product.py
+++ account_product_fiscal_classification/product.py
@@ -1,21 +1,24 @@
-# -*- en# -*- encoding: utf-8 -*-
-################################################################################
-#                                                                              #
-# Copyright (C) 2010  Raphaël Valyi, Renato Lima - Akretion                    #
-#                                                                              #
-#This program is free software: you can redistribute it and/or modify          #
-#it under the terms of the GNU Affero General Public License as published by   #
-#the Free Software Foundation, either version 3 of the License, or             #
-#(at your option) any later version.                                           #
-#                                                                              #
-#This program is distributed in the hope that it will be useful,               #
-#but WITHOUT ANY WARRANTY; without even the implied warranty of                #
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 #
-#GNU General Public License for more details.                                  #
-#                                                                              #
-#You should have received a copy of the GNU General Public License             #
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.         #
-################################################################################
+# -*- encoding: utf-8 -*-
+###############################################################################
+#
+#   account_product_fiscal_classification for OpenERP
+#   Copyright (C) 2010-TODAY Akretion <http://www.akretion.com>
+#     @author Renato Lima <renato.lima@akretion.com>
+#     @author Raphaël Valyi <rvalyi@akretion.com>
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
 
 from osv import fields, osv
 
