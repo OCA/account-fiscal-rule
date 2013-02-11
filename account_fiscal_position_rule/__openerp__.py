9c24505c23647f4cbc318a34433305d277d8e068
Renato Lima <renatonlima@gmail.com>
2013-02-11 20:44:58 -0200

    added new line in __openerp__.py

--- account_fiscal_position_rule/__openerp__.py
+++ account_fiscal_position_rule/__openerp__.py
@@ -21,6 +21,7 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
+
 {
     'name': 'Account Fiscal Position Rule',
     'version': '1.1',
