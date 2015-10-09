09dfae24ccc34805c56cd614735771b236a1bf25
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 09:59:26 +0200

    [UPD] prefix versions with 8.0

--- account_fiscal_position_rule_sale/__openerp__.py
+++ account_fiscal_position_rule_sale/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'Account Fiscal Position Rule Sale',
-    'version': '1.1.1',
+    'version': '8.0.1.1.1',
     'category': 'Generic Modules/Accounting',
     'description': """Include a rule to decide the
     correct fiscal position for Sale""",
