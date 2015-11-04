b551b0f56608b583a9bdf2d56c5d64950fa944ca
Renato Lima <renatonlima@gmail.com>
2015-11-04 15:21:32 -0200

    added tests

--- account_product_fiscal_classification/__openerp__.py
+++ account_product_fiscal_classification/__openerp__.py
@@ -41,7 +41,9 @@
     ],
     'demo': [
         'demo/account_tax.yml',
+        'demo/account_tax_template.yml',
         'demo/account_product_fiscal_classification.yml',
+        'demo/account_product_fiscal_classification_template.yml',
         'demo/product_template.yml',
         'demo/res_company.yml',
         'demo/res_groups.yml',
