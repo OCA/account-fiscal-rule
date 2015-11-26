d76680b2c8520681827d871eae190ee531f45da1
Magno Costa - Akretion <magno.costa@akretion.com.br>
2015-11-26 17:59:50 -0200

    Models migration and add license header.
    Removed view because onchange in the new API is defined by decorator.
    
    Add tests.
    
    [MIG] Removed view.
    
    [MIG] Fix version of the module.
    
    Change Lisence Header to new style and change the name of YAML folder.
    
    [FIX] In Fiscal Position field auto_apply need to set True to work mapping.
    
    Included README.rst
    
    [REF] Merge the onchanges.

--- account_fiscal_position_rule_sale/__openerp__.py
+++ account_fiscal_position_rule_sale/__openerp__.py
@@ -1,32 +1,14 @@
-# -*- encoding: utf-8 -*-
-###############################################################################
-#
-#   account_fiscal_position_rule_sale for OpenERP
+# -*- coding: utf-8 -*-
 #   Copyright (C) 2009-TODAY Akretion <http://www.akretion.com>
 #     @author Renato Lima <renato.lima@akretion.com>
 #   Copyright 2012 Camptocamp SA
 #     @author: Guewen Baconnier
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Affero General Public License as
-#   published by the Free Software Foundation, either version 3 of the
-#   License, or (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Affero General Public License for more details.
-#
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 {
     'name': 'Account Fiscal Position Rule Sale',
-    'version': '8.0.1.1.1',
+    'version': '9.0.1.0.0',
     'category': 'Generic Modules/Accounting',
-    'description': """Include a rule to decide the
-    correct fiscal position for Sale""",
     'author': "Akretion,Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'website': 'http://www.akretion.com',
@@ -35,9 +17,11 @@
         'sale',
     ],
     'data': [
-        'views/sale_view.xml',
         'security/ir.model.access.csv',
     ],
     'demo': [],
-    'installable': False,
+    'test': [
+        'test/account_fiscal_position_rule_sale.yml',
+    ],
+    'installable': True,
 }
--- account_fiscal_position_rule_sale/models/__init__.py
+++ account_fiscal_position_rule_sale/models/__init__.py
@@ -1,2 +1,7 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+#   Copyright (C) 2009 Akretion <http://www.akretion.com>
+#     @author Renato Lima <renato.lima@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
 from . import sale
+
