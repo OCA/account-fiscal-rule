13569628f502f68c6ad67da6326ad94f15f2b911
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-03-28 00:53:21 +0200

    [ADD] feature to restrict fiscal setting on product depending on its category; [ADD] some description;

--- account_product_fiscal_classification/__openerp__.py
+++ account_product_fiscal_classification/__openerp__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2014-Today GRAP (http://www.grap.coop)
+# Copyright (C) 2016-Today La Louve (<http://www.lalouve.net/>)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Account Product - Fiscal Classification',
     'summary': 'Simplify taxes management for products',
-    'version': '8.0.2.1.0',
+    'version': '8.0.3.0.0',
     'category': 'Accounting',
     'author': 'Akretion,GRAP,Odoo Community Association (OCA)',
     'website': 'https://odoo-community.org/',
@@ -18,6 +19,7 @@
         'security/ir_model_access.yml',
         'views/view_product_template.xml',
         'views/action.xml',
+        'views/view_product_category.xml',
         'views/view_account_product_fiscal_classification.xml',
         'views/view_wizard_change_fiscal_classification.xml',
         'views/menu.xml',
@@ -26,6 +28,7 @@
         'demo/account_tax.xml',
         'demo/account_product_fiscal_classification.xml',
         'demo/product_template.xml',
+        'demo/product_category.xml',
         'demo/res_company.xml',
         'demo/res_groups.xml',
         'demo/res_users.xml',
---
+++ account_product_fiscal_classification/model/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from . import product_template
+from . import product_category
+from . import account_product_fiscal_classification
+from . import wizard_change_fiscal_classification
