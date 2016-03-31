c7b2f94844dc971e9f637306451082969b711a56
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-03-31 09:14:05 +0200

    [FIX] incorrect path after refactoring

--- account_product_fiscal_classification/__init__.py
+++ account_product_fiscal_classification/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2014-Today GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import model
+from . import models
 
 import logging
 from openerp import api, SUPERUSER_ID
