693a408f4d1de9a84a23d235fa801a7e962c1bf8
Magno Barcelo da Costa <magno.costa@akretion.com.br>
2016-10-06 17:45:51 -0300

    Change field Fiscal Position to be required. (#53)
    [IMP] account_fiscal_position_rule: Change field Fiscal Position to be required

--- account_fiscal_position_rule/models/account_invoice.py
+++ account_fiscal_position_rule/models/account_invoice.py
@@ -1,24 +1,8 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
-#   account_fiscal_position_rule for OpenERP
-#   Copyright (C) 2009-TODAY Akretion <http://www.akretion.com>
-#     @author Sébastien BEAU <sebastien.beau@akretion.com>
-#     @author Renato Lima <renato.lima@akretion.com>
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Affero General Public License as
-#   published by the Free Software Foundation, either version 3 of the
-#   License, or (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Affero General Public License for more details.
-#
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# Copyright (C) 2009-TODAY Akretion <http://www.akretion.com>
+#   @author Sébastien BEAU <sebastien.beau@akretion.com>
+#   @author Renato Lima <renato.lima@akretion.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import models, api
 
