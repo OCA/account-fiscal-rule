abe4e23de0f30265480e611ee9831bee0fdf6627
Yogesh Kushwaha <yogeshkushwaha4@gmail.com>
2016-11-23 12:52:46 -0200

    set version 10.0 for account_fiscal_position_rule

--- account_fiscal_position_rule/__manifest__.py
+++ account_fiscal_position_rule/__manifest__.py
@@ -24,7 +24,7 @@
 
 {
     'name': 'Account Fiscal Position Rule',
-    'version': '8.0.1.2.0',
+    'version': '10.0.1.2.0',
     'category': 'Generic Modules/Accounting',
     'description': """Include a rule to decide the correct fiscal position""",
     'author': "Akretion,Odoo Community Association (OCA)",
