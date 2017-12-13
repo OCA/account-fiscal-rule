edb216ccb81594c96b5e1fa7c43b538323287f22
renato <renato.lima@akretion.com.br>
2017-12-13 14:11:01 -0200

    [MIGR] remove blank spaces from header files

--- account_fiscal_position_rule_purchase/models/purchase.py
+++ account_fiscal_position_rule_purchase/models/purchase.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-#   Copyright (C) 2009-TODAY Akretion <http://www.akretion.com>
-#     @author Renato Lima <renato.lima@akretion.com>
+# Copyright (C) 2009-TODAY Akretion <http://www.akretion.com>
+#   @author Renato Lima <renato.lima@akretion.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import models, api
