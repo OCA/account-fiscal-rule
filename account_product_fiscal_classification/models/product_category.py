a470299e5c31bc4d84218842f7464304136be6df
renato <renato.lima@akretion.com.br>
2017-12-13 14:29:50 -0200

    [MIGR] fix import

--- account_product_fiscal_classification/models/product_category.py
+++ account_product_fiscal_classification/models/product_category.py
@@ -3,8 +3,8 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api, _
-from openerp.exceptions import ValidationError
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 
 
 class ProductCategory(models.Model):
