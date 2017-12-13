7e49355497f7d5874c31eb6a18f1e81bdcdc8d4b
renato <renato.lima@akretion.com.br>
2017-12-13 14:07:13 -0200

    [MIGR] update metafile
    [MIGR] import

--- account_fiscal_position_rule_sale/models/sale.py
+++ account_fiscal_position_rule_sale/models/sale.py
@@ -7,7 +7,7 @@
 #     @author: Guewen Baconnier
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from openerp import models, api
+from odoo import models, api
 
 
 class SaleOrder(models.Model):
