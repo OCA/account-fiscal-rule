821e0a2febb138941d25688cffc2b9874df7760d
Joren Van Onder <jov@odoo.com>
2018-10-15 13:26:28 -0700

    [FIX] propagate the chosen address from the SO to the invoice
    If the SO is taxed on delivery address, the invoice should be as well.

--- avatax_connector/wizard/__init__.py
+++ avatax_connector/wizard/__init__.py
@@ -2,5 +2,6 @@
 
 from . import avalara_salestax_ping
 from . import avalara_salestax_address_validate
+from . import sale_advance_payment_inv
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ avatax_connector/wizard/sale_advance_payment_inv.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+
+class SaleAdvancePaymentInv(models.TransientModel):
+    _inherit = "sale.advance.payment.inv"
+    _description = "Sales Advance Payment Invoice"
+
+    @api.multi
+    def _create_invoice(self, order, so_line, amount):
+        invoice = super(SaleAdvancePaymentInv, self)._create_invoice(order, so_line, amount)
+        invoice.write({
+            'exemption_code': order.exemption_code or '',
+            'exemption_code_id': order.exemption_code_id.id or False,
+            'location_code': order.location_code or '',
+            'tax_on_shipping_address': order.tax_add_shipping,
+        })
+        invoice.compute()
+        return invoice
