44cafd91ca54ed6b057e139e02c2af58df7f491d
Renato Lima <renato.lima@akretion.com.br>
2018-12-26 13:05:34 -0200

    [MIGR] migrate object and split files

--- account_fiscal_position_rule/models/account_invoice.py
+++ account_fiscal_position_rule/models/account_invoice.py
@@ -10,24 +10,8 @@ from odoo import models, api
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    def _fiscal_position_map(self, **kwargs):
-        ctx = dict(self._context)
-        ctx.update({'use_domain': ('use_invoice', '=', True)})
-        return self.env['account.fiscal.position.rule'].with_context(
-            ctx).apply_fiscal_mapping(**kwargs)
-
-    @api.onchange('partner_id', 'company_id')
-    def _onchange_partner_id(self):
-
-        super(AccountInvoice, self)._onchange_partner_id()
-
-        if self.partner_id and self.company_id:
-            kwargs = {
-                'company_id': self.company_id,
-                'partner_id': self.partner_id,
-                'partner_invoice_id': self.partner_id,
-                'partner_shipping_id': self.partner_id,
-            }
-            obj_fiscal_position = self._fiscal_position_map(**kwargs)
-            if obj_fiscal_position is not False:
-                self.fiscal_position_id = obj_fiscal_position.id
+    #@api.onchange('partner_id', 'company_id')
+    #def _onchange_partner_id(self):
+    #    ctx = dict(self._context)
+    #    ctx.update({'use_domain': ('use_invoice', '=', True)})
+    #    self.with_context(ctx)
--- account_fiscal_position_rule/wizard/__init__.py
+++ account_fiscal_position_rule/wizard/__init__.py
@@ -3,5 +3,4 @@
 #   @author Renato Lima <renato.lima@akretion.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import models
-from . import wizard
+from . import wizard_account_fiscal_position_rule
