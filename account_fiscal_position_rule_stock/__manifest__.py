6b290ebaf766d7001d969f0a65e7c6c0b9d2d39d
Magno Costa <magno.costa@akretion.com.br>
2019-01-09 15:23:51 -0200

    [MIG][10.0] Make module installable and include dependency from moduule account_invoice_from_picking.

--- account_fiscal_position_rule_stock/__manifest__.py
+++ account_fiscal_position_rule_stock/__manifest__.py
@@ -1,27 +1,11 @@
-# coding: utf-8
-###############################################################################
-#
-#   account_fiscal_position_rule_stock for OpenERP
-#   Copyright (C) 2009 Akretion <http://www.akretion.com>
-#     @author Renato Lima <renato.lima@akretion.com>
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU Affero General Public License as
-#   published by the Free Software Foundation, either version 3 of the
-#   License, or (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU Affero General Public License for more details.
-#
-#   You should have received a copy of the GNU Affero General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# -*- coding: utf-8 -*-
+# @ 2009 Akretion - www.akretion.com.br -
+# @author Renato Lima <renato.lima@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Account Fiscal Position Rule Stock',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Generic Modules/Accounting',
     'author': "Akretion,Odoo Community Association (OCA)",
     'license': 'AGPL-3',
@@ -29,6 +13,7 @@
     'depends': [
         'account_fiscal_position_rule',
         'stock_account',
+        'account_invoice_from_picking',
     ],
     'data': [
         'views/stock_picking.xml',
@@ -39,5 +24,5 @@
         'tests/sale_order_test.yml',
         'tests/invoice_from_picking_test.yml'
     ],
-    'installable': False,
+    'installable': True,
 }
