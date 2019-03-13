16773cc2ec9d7556d19baa281f17fea8e4646a92
Pierrick Brun <pierrick.brun@akretion.com>
2019-03-13 17:06:28 +0100

    [MIG] 12.0

--- account_product_fiscal_classification/__manifest__.py
+++ account_product_fiscal_classification/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2014-Today GRAP (http://www.grap.coop)
 # Copyright (C) 2016-Today La Louve (<http://www.lalouve.net/>)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
@@ -7,36 +6,22 @@
 {
     'name': 'Account Product - Fiscal Classification',
     'summary': 'Simplify taxes management for products',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting',
     'author': 'Akretion,GRAP,La Louve,Odoo Community Association (OCA)',
-    'website': 'https://odoo-community.org/',
+    'website': 'https://github.com/OCA/account-fiscal-rule',
     'license': 'AGPL-3',
     'depends': [
         'account',
-        'l10n_generic_coa',
     ],
     'data': [
         'security/ir_rule.xml',
-        'security/ir_model_access.yml',
+        'security/ir.model.access.csv',
         'views/view_related.xml',
         'views/action.xml',
         'views/view.xml',
         'views/menu.xml',
         'views/view_product_category.xml',
     ],
-    'demo': [
-        'demo/account_tax.yml',
-        'demo/account_tax_template.yml',
-        'demo/account_product_fiscal_classification.yml',
-        'demo/account_product_fiscal_classification_template.yml',
-        'demo/product_category.xml',
-        'demo/product_template.yml',
-        'demo/res_company.yml',
-        'demo/res_groups.yml',
-        'demo/res_users.yml',
-    ],
-    'post_init_hook':
-        'create_fiscal_classification_from_product_template',
     'installable': True,
 }
