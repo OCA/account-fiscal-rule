d845b06f86b06f6ea871858783c9dba2204d493c
Magno Costa <magno.costa@akretion.com.br>
2019-07-22 09:32:56 -0300

    [10.0][MIG] PEP8.

--- account_fiscal_position_rule_stock/wizards/stock_invoice_onshipping.py
+++ account_fiscal_position_rule_stock/wizards/stock_invoice_onshipping.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2019-Today: Odoo Community Association (OCA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api, _
+from odoo import models, fields, api
 
 
 class StockInvoiceOnshipping(models.TransientModel):
