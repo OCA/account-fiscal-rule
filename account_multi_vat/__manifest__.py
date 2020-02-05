f6f93e6ebcabc37c03b6a4e6bfe0e81db7b7281f
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2020-02-05 17:07:52 +0100

    [ADD] Account Multi VAT refs #101
    [UPD] Update account_multi_vat.pot
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: account-fiscal-rule-13.0/account-fiscal-rule-13.0-account_multi_vat
    Translate-URL: https://translation.odoo-community.org/projects/account-fiscal-rule-13-0/account-fiscal-rule-13-0-account_multi_vat/

---
+++ account_multi_vat/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Multi Vat",
+    "summary": """
+        Allows setting multiple VAT numbers on any partner and select the right one
+        depending on the fiscal position and delivery address of the invoice.""",
+    "version": "13.0.1.0.0",
+    "development_status": "Beta",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "maintainers": ["ThomasBinsfeld"],
+    "website": "https://github.com/OCA/account-fiscal-rule",
+    "depends": ["account", "base_vat", "partner_identification"],
+    "data": [
+        "views/account_tax.xml",
+        "data/res_partner_id_category.xml",
+        "views/account_move.xml",
+        "views/res_partner.xml",
+        "views/account_fiscal_position.xml",
+        "views/report_invoice.xml",
+    ],
+}
