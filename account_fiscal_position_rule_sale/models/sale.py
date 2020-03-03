411782db80acefc9b1fe80ebfafaf8c7a313673c
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2020-03-03 16:51:13 +0100

    [MIG] Account Fiscal Position Rule Sale to 13.0

--- account_fiscal_position_rule_sale/models/sale.py
+++ account_fiscal_position_rule_sale/models/sale.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 #   Copyright (C) 2009-TODAY Akretion <http://www.akretion.com>
 #     @author Sébastien BEAU <sebastien.beau@akretion.com>
 #     @author Renato Lima <renato.lima@akretion.com>
@@ -7,29 +6,35 @@
 #     @author: Guewen Baconnier
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo import models, api
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     def _fiscal_position_map(self, **kwargs):
         ctx = dict(self._context)
-        ctx.update({'use_domain': ('use_sale', '=', True)})
-        return self.env['account.fiscal.position.rule'].with_context(
-            ctx).apply_fiscal_mapping(**kwargs)
+        ctx.update({"use_domain": ("use_sale", "=", True)})
+        return (
+            self.env["account.fiscal.position.rule"]
+            .with_context(ctx)
+            .apply_fiscal_mapping(**kwargs)
+        )
 
-    @api.onchange('partner_id', 'partner_invoice_id',
-                  'partner_shipping_id', 'company_id')
-    def onchange_fiscal_position_map(self):
-
-        kwargs = {
-            'company_id': self.company_id,
-            'partner_id': self.partner_id,
-            'partner_invoice_id': self.partner_invoice_id,
-            'partner_shipping_id': self.partner_shipping_id,
+    def _prepare_fiscal_position_map_kwargs(self):
+        self.ensure_one()
+        return {
+            "company_id": self.company_id,
+            "partner_id": self.partner_id,
+            "partner_invoice_id": self.partner_invoice_id,
+            "partner_shipping_id": self.partner_shipping_id,
         }
 
+    @api.onchange(
+        "partner_id", "partner_invoice_id", "partner_shipping_id", "company_id"
+    )
+    def onchange_fiscal_position_map(self):
+        kwargs = self._prepare_fiscal_position_map_kwargs()
         obj_fiscal_position = self._fiscal_position_map(**kwargs)
         if obj_fiscal_position:
             self.fiscal_position_id = obj_fiscal_position.id
---
+++ account_fiscal_position_rule_sale/readme/CONTRIBUTORS.rst
@@ -0,0 +1,6 @@
+* Renato Lima <renato.lima@akretion.com>
+* Sébastien BEAU sebastien.beau@akretion.com>
+* Guewen Baconnier
+* Raphaël Valyi <rahael.valyi@akretion.com>
+* Magno Costa <magno.costa@akretion.com>
+* Hugo Santos <hugo.santos@factorlibre.com>
