dae7780cf4554803d9a1f6ee05b670e3a3a7c5df
Daniel Reis <dreis@opensourceintegrators.com>
2020-05-29 09:14:39 +0100

    [REF] Prepare for OCA submission, rename module

---
+++ account_avatax/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    "name": "Taxes using Avalara Avatax API",
+    "version": "13.0.1.0.0",
+    "author": "Open Source Integrators, Fabrice Henrion, Odoo SA,"
+    " Odoo Community Association (OCA)",
+    "summary": "Automatic Tax application using the Avalara Avatax Service",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "depends": ["account", "sale_stock", "base_geolocalize"],
+    "data": [
+        "security/avalara_salestax_security.xml",
+        "security/ir.model.access.csv",
+        "data/avalara_salestax_data.xml",
+        "data/avalara_salestax_exemptions.xml",
+        "wizard/avalara_salestax_ping_view.xml",
+        "wizard/avalara_salestax_address_validate_view.xml",
+        "views/avalara_salestax_view.xml",
+        "views/partner_view.xml",
+        "views/product_view.xml",
+        "views/account_move_action.xml",
+        "views/account_move_view.xml",
+        "views/account_tax_view.xml",
+        "views/account_fiscal_position_view.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "external_dependencies": {"python": ["Avalara"]},
+}
