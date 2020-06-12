67af31d0cc5421a79652816d8867afd225d962cd
Daniel Reis <dreis@opensourceintegrators.com>
2020-06-12 11:57:36 +0100

    [IMP] avatax: don't error when getting config for empty company

--- account_avatax/models/res_company.py
+++ account_avatax/models/res_company.py
@@ -10,17 +10,19 @@ class Company(models.Model):
 
     def get_avatax_config_company(self):
         """ Returns the AvaTax configuration for the Company """
-        self.ensure_one()
-        AvataxConfig = self.env["avalara.salestax"]
-        res = AvataxConfig.search(
-            [("company_id", "=", self.id), ("disable_tax_calculation", "=", False)]
-        )
-        if len(res) > 1:
-            _LOGGER.warn(
-                _("Company %s has too many Avatax configurations!"), self.display_name
+        if self:
+            self.ensure_one()
+            AvataxConfig = self.env["avalara.salestax"]
+            res = AvataxConfig.search(
+                [("company_id", "=", self.id), ("disable_tax_calculation", "=", False)]
             )
-        if len(res) < 1:
-            _LOGGER.warn(
-                _("Company %s has no Avatax configuration."), self.display_name
-            )
-        return res and res[0]
+            if len(res) > 1:
+                _LOGGER.warn(
+                    _("Company %s has too many Avatax configurations!"),
+                    self.display_name,
+                )
+            if len(res) < 1:
+                _LOGGER.warn(
+                    _("Company %s has no Avatax configuration."), self.display_name
+                )
+            return res and res[0]
