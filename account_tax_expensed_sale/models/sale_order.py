3fb37dd95d9f5923bf436736377202d401432283
Daniel Reis <dreis@opensourceintegrators.com>
2020-06-18 22:03:55 +0100

    [REF] account_tax_expensed_sale: better field computations

--- account_tax_expensed_sale/models/sale_order.py
+++ account_tax_expensed_sale/models/sale_order.py
@@ -1,10 +1,7 @@
 # Copyright 2020 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-# We are extending a base methods that does not follow conventions
-# pylint: disable=C8108
-
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
@@ -12,16 +9,14 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     amount_tax_expense = fields.Monetary(
-        string="Tax Expense",
-        readonly=True,
-        compute="_amount_all",
+        string="Tax Expense", compute="_compute_amount_tax_expense",
     )
 
-    def _amount_all(self):
-        super()._amount_all()
+    @api.depends("order_line.tax_expense")
+    def _compute_amount_tax_expense(self):
+        """ Compute Expensed Tax """
         for order in self:
             round_curr = order.currency_id.round
-            order.amount_tax_expense = sum(
-                round_curr(line.tax_expense)
-                for line in order.order_line
+            order.amount_tax_expense = round_curr(
+                sum([line.tax_expense for line in order.order_line])
             )
--- account_tax_expensed_sale/models/sale_order_line.py
+++ account_tax_expensed_sale/models/sale_order_line.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
@@ -17,21 +17,44 @@ class SaleOrderLine(models.Model):
     tax_total = fields.Monetary(
         string="Tax",
         readonly=True,
-        compute="_compute_amount",
+        compute="_compute_tax_total",
         help="Total tax amount, both collected from the customer "
         "and expensed by the company.",
     )
 
+    def _get_expensed_tax_price(self):
+        """
+        Returns the Base Amount to use for Expensed Tax
+        Can be extended to implement specific taxes.
+        """
+        self.ensure_one()
+        return self.price_unit * (1 - (self.discount or 0.0) / 100.0)
+
     def _compute_amount(self):
-        """ Compute the tax_expense field """
+        """
+        Compute the Tax Expense field
+        Needs to be done on the same method computing the tax amount.
+        """
         super()._compute_amount()
         for line in self:
-            taxes = {}
-            if any(x.is_expensed_tax for x in line.tax_id):
-                price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            line.tax_expense = 0
+            has_expensed_tax = any(x.is_expensed_tax for x in line.tax_id)
+            if has_expensed_tax:
+                price = line._get_expensed_tax_price()
                 taxes = line.tax_id.compute_all(
-                    price, line.order_id.currency_id, line.product_uom_qty,
+                    price,
+                    line.order_id.currency_id,
+                    line.product_uom_qty,
                     product=line.product_id,
-                    partner=line.order_id.partner_shipping_id)
-            line.tax_expense = taxes.get('total_expense') or 0
+                    partner=line.order_id.partner_shipping_id,
+                )
+                line.tax_expense = taxes.get["total_expense"]
+
+    @api.depends("price_tax", "tax_expense")
+    def _compute_tax_total(self):
+        """
+        Compute the Tax Total field,
+        as the sum of included taxes and expensed taxes
+        """
+        for line in self:
             line.tax_total = line.price_tax + line.tax_expense
