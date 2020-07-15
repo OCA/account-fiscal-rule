90963d74dee273119eb3f50bdd35bc0d001839bc
Daniel Reis <dreis@opensourceintegrators.com>
2020-07-15 17:05:18 +0100

    [FIX] account_avatax: force using the tax amounts computed by Avatax API

--- account_avatax/models/account_tax.py
+++ account_avatax/models/account_tax.py
@@ -1,6 +1,7 @@
 import logging
 
 from odoo import _, api, fields, models
+from odoo.tools.float_utils import float_compare
 
 _logger = logging.getLogger(__name__)
 
@@ -25,10 +26,6 @@ class AccountTax(models.Model):
 
     @api.model
     def get_avalara_tax(self, tax_rate, doc_type):
-        """
-        Given a tax rate, returns the corresponding Tax record.
-        The Tax is created if it doesn't exist yet.
-        """
         if tax_rate:
             tax = self.with_context(active_test=False).search(
                 self._get_avalara_tax_domain(tax_rate, doc_type), limit=1
@@ -40,7 +37,49 @@ class AccountTax(models.Model):
                     self._get_avalara_tax_domain(0, doc_type), limit=1
                 )
                 tax = tax_template.sudo().copy(default={"amount": tax_rate})
+                # If you get a unique constraint error here,
+                # check the data for your existing Avatax taxes.
                 tax.name = self._get_avalara_tax_name(tax_rate, doc_type)
             return tax
         else:
             return self
+
+    def compute_all(
+        self,
+        price_unit,
+        currency=None,
+        quantity=1.0,
+        product=None,
+        partner=None,
+        is_refund=False,
+        handle_price_include=True,
+    ):
+        """
+        Adopted as the central point to inject custom tax computations.
+        Avatax logic is triggered if the "avatax_invoice" is set in the context.
+        To find the Avatax amount, we search an Invoice line with the same
+        quantity, price and product.
+        """
+        res = super().compute_all(
+            price_unit,
+            currency,
+            quantity,
+            product,
+            partner,
+            is_refund,
+            handle_price_include,
+        )
+        avatax_invoice = self.env.context.get("avatax_invoice")
+        if avatax_invoice:
+            digits = 6
+            invoice_line = avatax_invoice.invoice_line_ids.filtered(
+                lambda x: float_compare(x.price_unit, -price_unit, digits)
+                and float_compare(x.quantity, quantity, digits)
+                and x.product_id == product
+            )[:1]
+            avatax_amount = -invoice_line.avatax_amt_line
+            for tax_item in res["taxes"]:
+                if tax_item["amount"] != 0:
+                    tax_item["amount"] = avatax_amount
+            res["total_included"] = res["total_excluded"] + avatax_amount
+        return res
