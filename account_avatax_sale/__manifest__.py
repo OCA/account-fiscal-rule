7258a72376c3d0bee9a3f5e5eec3bd13073b5a83
Don Kendall <kendall@donkendall.com>
2020-11-26 17:41:17 -0500

    [IMP] account_avatax_sale: black, isort, prettier

--- account_avatax_sale/__manifest__.py
+++ account_avatax_sale/__manifest__.py
@@ -1,10 +1,11 @@
 {
     "name": "Avalara Avatax Connector for Sales Orders",
-    "version": "13.0.2.4.1",
+    "version": "14.0.1.0.0",
     "author": "Fabrice Henrion, Odoo Community Association (OCA)",
     "summary": "Sales Orders with automatic Tax application using Avatax",
     "license": "AGPL-3",
     "category": "Accounting",
+    "website": "https://github.com/OCA/account-fiscal-rule",
     "depends": ["account_avatax", "sale"],
     "data": ["views/sale_order_view.xml", "views/partner_view.xml"],
     "auto_install": True,
