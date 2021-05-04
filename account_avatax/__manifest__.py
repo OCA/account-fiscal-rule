6c42b4d8ee6e1b5a45c7b3d82cf1ebbc22540c27
Daniel Reis <dreis@opensourceintegrators.com>
2021-05-04 12:36:05 +0100

    [FIX] account_avatax: app store images

--- account_avatax/__manifest__.py
+++ account_avatax/__manifest__.py
@@ -21,7 +21,7 @@
         "views/account_tax_view.xml",
         "views/account_fiscal_position_view.xml",
     ],
-    "image": ["static/description/avatax_icon.png"],
+    "images": ["static/description/avatax_icon.png"],
     "installable": True,
     "application": True,
     "external_dependencies": {"python": ["Avalara"]},
