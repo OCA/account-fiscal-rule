6387701147d60f8d5c2b9ca993d42eccd171c114
Mayank Gosai <mgosai@ursainfosystems.com>
2021-05-12 16:44:27 -0400

    [FIX] account_avatax: have a separate Avatax tax template for each company

--- account_avatax/tests/__init__.py
+++ account_avatax/tests/__init__.py
@@ -1,1 +1,2 @@
+from . import test_account_tax
 from . import test_avatax
---
+++ account_avatax/tests/test_account_tax.py
@@ -0,0 +1,27 @@
+# Copyright 2021 Open Source Integrators
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+
+from odoo import exceptions
+from odoo.tests.common import SavepointCase
+
+
+class TestAvatax(SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.Tax = cls.env["account.tax"]
+        cls.company1 = cls.env.ref("base.main_company")
+        cls.company2 = cls.env["res.company"].create({"name": "Company 2"})
+
+    def test_get_avatax_tax_rate(self):
+        tax75 = self.Tax.get_avalara_tax(7.5, "out_invoice")
+        self.assertEqual(tax75.amount, 7.5)
+
+    def test_get_avatax_template(self):
+        tax = self.Tax.get_avalara_tax(0, "out_invoice")
+        self.assertEqual(tax.name, "AVATAX")
+
+    def test_get_avatax_template_missing(self):
+        with self.assertRaises(exceptions.UserError):
+            self.Tax.with_company(self.company2).get_avalara_tax(0, "out_invoice")
