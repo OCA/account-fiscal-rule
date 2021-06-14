716b03aae8a929d99d7fed87177ff3651cada23d
valen <valentin.vinagre@sygel.es>
2021-06-14 11:47:15 +0200

    [ADD] Account Fiscal Position Partner Type

---
+++ account_fiscal_position_partner_type/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Valentín Vinagre <valentin.vinagre@sygel.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Fiscal Position Partner Type",
+    "version": "13.0.0.0.1",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/OCA/account-fiscal-rule",
+    "author": "Sygel Technology," "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "development_status": "Production/Stable",
+    "depends": ["account"],
+    "data": [
+        "views/res_company.xml",
+        "views/res_partner.xml",
+        "views/account_fiscal_position.xml",
+    ],
+}
