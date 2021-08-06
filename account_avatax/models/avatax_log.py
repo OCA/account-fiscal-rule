8dcac93ce5f97c105d427629eecaaa67095a8da6
Atchuthan, Sodexis <atchuthan@sodexis.com>
2021-08-06 11:46:57 +0530

    [FIX] Singleton error

--- account_avatax/models/avatax_log.py
+++ account_avatax/models/avatax_log.py
@@ -25,37 +25,25 @@ class AvataxLog(models.Model):
 
     def avatax_api_call_counter(self):
         if not self.avatax_request:
-            self = self.search([])
-            call_count = len(
-                self.filtered(
-                    lambda p: p.create_date_time.date() == fields.date.today()
-                )
+            logs = self.search([]).filtered(
+                lambda p: p.create_date_time.date() == fields.date.today()
             )
             sales_call_count = len(
-                self.filtered(
-                    lambda p: p.create_date_time.date() == fields.date.today()
-                )
-                and self.avatax_type == "SalesOrder"
+                logs.filtered(lambda p: p.avatax_type == "SalesOrder")
             )
             invoices_call_count = len(
-                self.filtered(
-                    lambda p: p.create_date_time.date() == fields.date.today()
-                )
-                and self.avatax_type == "SalesInvoice"
+                logs.filtered(lambda p: p.avatax_type == "SalesInvoice")
             )
-            others_call_count = len(
-                self.filtered(
-                    lambda p: p.create_date_time.date() == fields.date.today()
-                )
-                and self.avatax_type == "others"
-            )
-        if call_count:
+            others_call_count = len(logs.filtered(lambda p: p.avatax_type == "others"))
+        if len(logs):
             self.env.ref(
                 "account_avatax.reaching_limit_avatax_api_call_email"
             ).with_context(
-                sales_call_count=sales_call_count.send_mail(self.id, force_send=True),
-                invoices_call_count=invoices_call_count.send_mail(
-                    self.id, force_send=True
-                ),
-                others_call_count=others_call_count.send_mail(self.id, force_send=True),
+                {
+                    "sales_call_count": sales_call_count,
+                    "invoices_call_count": invoices_call_count,
+                    "others_call_count": others_call_count,
+                }
+            ).send_mail(
+                logs[0].id, force_send=True
             )
