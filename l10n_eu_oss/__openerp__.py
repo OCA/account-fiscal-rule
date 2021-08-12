529e43650279098e1bde163618eaeca0d70960cc
Rodrigo <rodrigo.bonilla@factorlibre.com>
2021-08-12 20:13:42 +0200

    [FIX] l10n_eu_oss: In v8 it is necessary to create account_tax_code to assign them to taxes.
    If account_tax_code are not assigned to the taxes, they will not be passed to the accounting entries.

--- l10n_eu_oss/__openerp__.py
+++ l10n_eu_oss/__openerp__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "L10n EU OSS",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/l10n-spain",
     "author": "Sygel Technology," "Odoo Community Association (OCA)",
