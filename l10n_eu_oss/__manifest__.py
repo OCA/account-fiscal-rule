a385432c6b07ceba06a360c17ae0b8c98c25124f
Simone Rubino <sir@takobi.online>
2022-06-22 09:51:26 +0200

    [FIX] l10n_eu_oss: Website URL

--- l10n_eu_oss/__manifest__.py
+++ l10n_eu_oss/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "L10n EU OSS",
     "version": "12.0.1.0.0",
     "category": "Accounting & Finance",
-    "website": "https://github.com/OCA/l10n-spain",
+    "website": "https://github.com/OCA/account-fiscal-rule",
     "author": "Sygel Technology," "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
