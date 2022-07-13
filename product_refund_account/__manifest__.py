486f845217f85ea28355a9e5c92adb9aa3b53800
cubells <vicent@vcubells.net>
2022-07-13 18:24:57 +0200

    [ADD][12.0] product_refund_account: new module

---
+++ product_refund_account/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Trey, Kilobytes de Soluciones - Vicent Cubells
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Product Refund Account",
+    "summary": "Adds account to product to manage debit and credit notes",
+    "author": "Trey (www.trey.es), Odoo Community Association (OCA)",
+    "maintainers": ["cubells"],
+    "website": "https://www.github.com/oca/account-fiscal-rule",
+    "license": "AGPL-3",
+    "category": "Accounting & Finance",
+    "version": "12.0.1.0.0",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/product_category_views.xml",
+        "views/product_template_views.xml",
+    ],
+}
---
+++ product_refund_account/readme/USAGE.rst
@@ -0,0 +1,2 @@
+Once the modules is installed, you can set an account in `Accounting` tab,
+both for credit and for debit notes.
