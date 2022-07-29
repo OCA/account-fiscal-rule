2ed6c2ab25e7f46ed3f22e84b90070be97a36882
Atchuthan Ubendran <atchuthan@sodexis.com>
2022-07-29 19:10:48 +0530

    [14.0] account_avatax_exemption_base: change license to LGPL

--- account_avatax_exemption_base/models/partner.py
+++ account_avatax_exemption_base/models/partner.py
@@ -8,9 +8,3 @@ class ResPartner(models.Model):
     exemption_ids = fields.One2many(
         "res.partner.exemption", "partner_id", string="Avalara Exemptions"
     )
-    use_commercial_entity = fields.Boolean(compute="_compute_use_commercial_entity")
-
-    def _compute_use_commercial_entity(self):
-        avalara_salestax = self.env["avalara.salestax"].sudo().search([], limit=1)
-        for partner in self:
-            partner.use_commercial_entity = avalara_salestax.use_commercial_entity
