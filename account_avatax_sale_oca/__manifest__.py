0018f4972cfa7527412f058d96907e5e24e55854
Daniel Reis <dreis@opensourceintegrators.com>
2022-08-02 21:02:42 +0100

    account_avatax_sale_oca: renamed from account_sale_avatax

---
+++ account_avatax_sale_oca/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Avalara Avatax Certified Connector for Sales Orders",
+    "version": "15.0.1.1.0",
+    "author": "Open Source Integrators, Fabrice Henrion,"
+    "Sodexis, Odoo Community Association (OCA)",
+    "summary": "Sales Orders with automatic Tax application using Avatax",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "website": "https://github.com/OCA/account-fiscal-rule",
+    "depends": ["account_avatax_oca", "sale_stock"],
+    "data": [
+        "views/sale_order_view.xml",
+        "views/partner_view.xml",
+        "views/avalara_salestax_view.xml",
+    ],
+    "auto_install": True,
+    "development_status": "Production/Stable",
+    "maintainers": ["dreispt"],
+}
