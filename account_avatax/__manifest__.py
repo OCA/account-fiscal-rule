9be90e35d2dffe0f4773876d0feee159f9b9c3ef
Daniel Reis <dreis@opensourceintegrators.com>
2022-08-02 17:31:43 +0100

    [REF] account_avatax: transition module for account_avatax_oca

---
+++ account_avatax/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "OCA Transition Module for Avalara Avatax Connector",
+    "version": "15.0.1.1.0",
+    "author": "Open Source Integrators, Fabrice Henrion,"
+    "Sodexis, Odoo Community Association (OCA)",
+    "summary": "Easy migration from account_avatax to account_avatax_oca module",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "website": "https://github.com/OCA/account-fiscal-rule",
+    "depends": ["account_avatax_oca"],
+    "installable": True,
+    "application": True,
+    "development_status": "Production/Stable",
+    "maintainers": ["dreispt"],
+}
---
+++ account_avatax/readme/USAGE.rst
@@ -0,0 +1,2 @@
+To migrate to the new OCA module, first install ``account_avatax_oca`` and
+``account_avatax_sale_oca``, and then uninstall ``account_avatax``.
