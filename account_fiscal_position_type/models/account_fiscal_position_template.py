3ec585b8432c022ab4a233e00ec50d5045eb8bbb
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-10-17 21:41:08 +0200

    [MIG] port account_fiscal_position_type from 12.0 to 16.0 - remove check on res.partner, as the field 'customer' and 'supplier' disappeared - add new demo chart of account to avoid to depends on l10n_generic_coa that makes test failing in some configuration - refactor the algorithm, introducing suitable_fiscal_position_ids field (as Odoo does with suitable_journal_ids field) - Update documentation

--- account_fiscal_position_type/models/account_fiscal_position_template.py
+++ account_fiscal_position_type/models/account_fiscal_position_template.py
@@ -2,21 +2,16 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-
 from odoo import fields, models
+
+from .account_fiscal_position import AccountFiscalPosition
 
 
 class AccountFiscalPositionTemplate(models.Model):
     _inherit = "account.fiscal.position.template"
 
-    _TYPE_POSITION_USE_SELECTION = [
-        ("sale", "Sale"),
-        ("purchase", "Purchase"),
-        ("all", "All"),
-    ]
-
     type_position_use = fields.Selection(
         string="Position Application",
-        selection=_TYPE_POSITION_USE_SELECTION,
+        selection=AccountFiscalPosition._TYPE_POSITION_USE_SELECTION,
         default="all",
     )
