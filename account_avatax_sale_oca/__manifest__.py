41e57f8f81110e197d9bd131cb7cdf0f1c02cc13
Nikul-OSI <nchaudhary@opensourceintegrators.com>
2023-04-07 18:08:21 +0530

    account_avatax_sale_oca: black, isort, prettier

--- account_avatax_sale_oca/__manifest__.py
+++ account_avatax_sale_oca/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Avalara Avatax Certified Connector for Sales Orders",
-    "version": "15.0.2.0.0",
+    "version": "16.0.1.0.0",
     "author": "Open Source Integrators, Fabrice Henrion,"
     "Sodexis, Odoo Community Association (OCA)",
     "summary": "Sales Orders with automatic Tax application using Avatax",
