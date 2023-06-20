1df937c2f77aad2c2a8e32bdc14be478245b9d83
Christopher Ormaza <chris.ormaza@forgeflow.com>
2023-06-20 13:48:58 -0500

    account_avatax_website_sale

--- account_avatax_website_sale/controllers/main.py
+++ account_avatax_website_sale/controllers/main.py
@@ -4,7 +4,7 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 
 class AvataxWebsiteSale(WebsiteSale):
-    def payment(self, **post):
+    def shop_payment(self, **post):
         order = request.website.sale_get_order()
         order._avatax_compute_tax()
-        return super(AvataxWebsiteSale, self).payment(**post)
+        return super(AvataxWebsiteSale, self).shop_payment(**post)
