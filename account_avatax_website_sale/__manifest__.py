c3e36f45ab2f3e40645ad389287464c5fa65ba19
Christopher Ormaza <chris.ormaza@forgeflow.com>
2023-06-20 13:48:58 -0500

    [16.0][MIG] account_avatax_website_sale

--- account_avatax_website_sale/__manifest__.py
+++ account_avatax_website_sale/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Avalara Avatax Connector for Ecommerce",
-    "version": "14.0.2.0.0",
+    "version": "16.0.1.0.0",
     "author": "Cybernexus Solutions, Odoo Community Association (OCA)",
     "summary": "Ecommerce Sales Orders require tax recalculation prior to payment.",
     "license": "AGPL-3",
