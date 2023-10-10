53c9a24a3fc50e93794eb87ccdfc5c2923b6c52d
Daniel Reis <dreis@opensourceintegrators.com>
2023-10-10 19:39:26 +0100

    account_avatax_oca: fix unbalanced error on save

--- account_avatax_oca/models/account_move.py
+++ account_avatax_oca/models/account_move.py
@@ -125,26 +125,6 @@ class AccountMove(models.Model):
         "Also, it will show Tax based on shipping address button",
     )
 
-    @api.depends(
-        "line_ids.debit",
-        "line_ids.credit",
-        "line_ids.currency_id",
-        "line_ids.amount_currency",
-        "line_ids.amount_residual",
-        "line_ids.amount_residual_currency",
-        "line_ids.payment_id.state",
-        "avatax_amount",
-    )
-    def _compute_amount(self):
-        res = super()._compute_amount()
-        for inv in self:
-            if inv.avatax_amount:
-                inv.amount_tax = abs(inv.avatax_amount)
-                inv.amount_total = inv.amount_untaxed + inv.amount_tax
-                sign = inv.move_type in ["in_refund", "out_refund"] and -1 or 1
-                inv.amount_total_signed = inv.amount_total * sign
-        return res
-
     @api.depends("tax_on_shipping_address", "partner_id", "partner_shipping_id")
     def _compute_tax_address_id(self):
         for invoice in self:
