be605f3a53583b136258686d589f61e10d7e9a51
Daniel Reis <dreis@opensourceintegrators.com>
2023-10-28 13:05:07 +0100

    account_avatax_exemption: pre-commit fixes

--- account_avatax_exemption/__manifest__.py
+++ account_avatax_exemption/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Avatax Exemptions",
-    "version": "14.0.3.0.0",
+    "version": "14.0.3.0.1",
     "category": "Sales",
     "summary": """
         This application allows you to add exemptions to Avatax
