a9e85dd8a2cf756b09564db93382886375bd6942
ArnauCForgeFlow <arnau.cruz@forgeflow.com>
2023-11-24 16:28:43 +0100

    [IMP] account_multi_foreign_vat

--- account_multi_foreign_vat/tests/test_account_multi_foreign_vat.py
+++ account_multi_foreign_vat/tests/test_account_multi_foreign_vat.py
@@ -1,5 +1,5 @@
-from odoo.tests.common import TransactionCase
 from odoo.exceptions import ValidationError
+from odoo.tests.common import TransactionCase
 
 
 class TestAccountMultiForeignVat(TransactionCase):
@@ -22,14 +22,19 @@ class TestAccountMultiForeignVat(TransactionCase):
                 "foreign_vat": self.valid_vat_dk,
             }
         )
+
     def test_account_multi_foreign_vat(self):
         # Check that 2 fiscal position are created successfully
         self.assertTrue(self.fiscal_position_1)
         self.assertTrue(self.fiscal_position_2)
 
         # Check that both fiscal positions have the same foreign_vat and country_id
-        self.assertEqual(self.fiscal_position_2.foreign_vat, self.fiscal_position_1.foreign_vat)
-        self.assertEqual(self.fiscal_position_2.country_id, self.fiscal_position_1.country_id)
+        self.assertEqual(
+            self.fiscal_position_2.foreign_vat, self.fiscal_position_1.foreign_vat
+        )
+        self.assertEqual(
+            self.fiscal_position_2.country_id, self.fiscal_position_1.country_id
+        )
 
         # Check that you cannot enter an invalid vat
         with self.assertRaises(ValidationError):
