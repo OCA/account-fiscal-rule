1880bd221525b0d8cef9961d215cf068d9fed30f
Mourad <mourad.elhadj.mimoune@akretion.com>
2024-05-27 15:13:12 +0200

    [16.0] add account_ecotax_sale module

---
+++ account_ecotax_sale/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2014-2023 Akretion (http://www.akretion.com)
+#   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "sale Ecotax Management",
+    "summary": "Sale Ecotaxe",
+    "version": "16.0.2.0.0",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-fiscal-rule",
+    "category": "Localization/Account Taxes",
+    "license": "AGPL-3",
+    "depends": ["account_ecotax", "sale"],
+    "data": [
+        "views/sale_view.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+}
---
+++ account_ecotax_sale/models/__init__.py
@@ -0,0 +1,3 @@
+from . import sale_order_line_ecotax
+from . import sale_order_line
+from . import sale_order
