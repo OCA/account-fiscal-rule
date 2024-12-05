2e878e5358908d8a25517660c2ce84afda36c52d
kobros-tech <alkobroslymohamed@gmail.com>
2024-12-05 23:01:39 +0300

    [18.0][MIG] account_avatax_oca_log: Migration to 18.0

--- account_avatax_oca_log/__manifest__.py
+++ account_avatax_oca_log/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Account Avatax OCA Log",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Open Source Integrators, ForgeFlow, Odoo Community Association (OCA)",
     "summary": "Add Logs to Avatax calls",
     "license": "AGPL-3",
