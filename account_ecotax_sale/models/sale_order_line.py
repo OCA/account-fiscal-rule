a0ee23ea6f664061a120264db294ba0356ab3fc9
Florian da Costa <florian.dacosta@akretion.com>
2025-01-16 17:17:39 +0100

    [IMP] Convert onchange to compute to create sale ecotax lines to improve reliability (when sol is not created/update from UI)

--- account_ecotax_sale/models/sale_order_line.py
+++ account_ecotax_sale/models/sale_order_line.py
@@ -2,7 +2,7 @@
 #   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import Command, api, fields, models
 
 
 class SaleOrderLine(models.Model):
@@ -12,6 +12,9 @@ class SaleOrderLine(models.Model):
     ecotax_line_ids = fields.One2many(
         "sale.order.line.ecotax",
         "sale_order_line_id",
+        compute="_compute_ecotax_line_ids",
+        store=True,
+        readonly=False,
         string="Ecotax lines",
         copy=True,
     )
@@ -42,26 +45,29 @@ class SaleOrderLine(models.Model):
             line.subtotal_ecotax = subtotal
             line.ecotax_amount_unit = amount_unit
 
-    @api.onchange("product_id")
-    def _onchange_product_ecotax_line(self):
+    def _get_new_vals_list(self):
+        self.ensure_one()
+        new_vals_list = [
+            Command.create(
+                {
+                    "classification_id": ecotaxline_prod.classification_id.id,
+                    "force_amount_unit": ecotaxline_prod.force_amount,
+                }
+            )
+            for ecotaxline_prod in self.product_id.all_ecotax_line_product_ids
+        ]
+        return new_vals_list
+
+    @api.depends("product_id")
+    def _compute_ecotax_line_ids(self):
         """Unlink and recreate ecotax_lines when modifying the product_id."""
-        self.ecotax_line_ids.unlink()
-        if self.product_id:
-            ecotax_cls_vals = []
-            for ecotaxline_prod in self.product_id.all_ecotax_line_product_ids:
-                classif_id = ecotaxline_prod.classification_id.id
-                forced_amount = ecotaxline_prod.force_amount
-                ecotax_cls_vals.append(
-                    (
-                        0,
-                        0,
-                        {
-                            "classification_id": classif_id,
-                            "force_amount_unit": forced_amount,
-                        },
-                    )
-                )
-            self.ecotax_line_ids = ecotax_cls_vals
+        for line in self:
+            delete_vals_list = [
+                Command.delete(taxline.id) for taxline in line.ecotax_line_ids
+            ]
+            new_vals_list = line._get_new_vals_list()
+            update = new_vals_list + delete_vals_list
+            line.ecotax_line_ids = update
 
     def edit_ecotax_lines(self):
         view = {
