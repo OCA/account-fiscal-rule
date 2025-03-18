007fb4ffa5e9adea1d25a01bf94718c4f94f120b
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-03-18 12:06:03 +0100

    [ADD] account_product_fiscal_classification_sale_expense: new module to hide sale taxes on expense category form view. (product.product)

---
+++ account_product_fiscal_classification_sale_expense/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2025-Today GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Product - Fiscal Classification - Sale Expense",
+    "summary": "Glue module between Sale Expense and fiscal classification",
+    "version": "16.0.1.1.0",
+    "category": "Accounting",
+    "author": "GRAP,Odoo Community Association (OCA)",
+    "maintainers": ["legalsylvain"],
+    "website": "https://github.com/OCA/account-fiscal-rule",
+    "license": "AGPL-3",
+    "depends": ["account_product_fiscal_classification", "sale_expense"],
+    "excludes": ["product_tax_multicompany_default"],
+    "data": ["views/view_product_product.xml"],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ account_product_fiscal_classification_sale_expense/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This module extends the OCA 'Product Fiscal Classification' module to make
+Sale Expense product form view working, if 'Sale Expense' is installed.
+
+With that glue module, sale taxes are readonly on expense category form view.
