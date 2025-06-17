57dfc82967d2ef696c8a7a69056feaa989e10b6b
Mourad <mourad.elhadj.mimoune@akretion.com>
2025-06-17 16:32:16 +0200

    [MIG] account_ecotax_sale: Migration to 18.0

--- account_ecotax_sale/models/sale_order_line.py
+++ account_ecotax_sale/models/sale_order_line.py
@@ -1,4 +1,4 @@
-# © 2015 -2023 Akretion (http://www.akretion.com)
+# © FROM 2015 Akretion (http://www.akretion.com)
 #   @author Mourad EL HADJ MIMOUNE <mourad.elhadj.mimoune@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
