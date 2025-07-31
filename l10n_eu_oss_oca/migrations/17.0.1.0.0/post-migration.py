c4db5b9d8ab3e62bf53de222efce4253c9063441
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2025-07-31 10:12:59 +0200

    [FIX] l10n_eu_oss_oca: set country on tax groups

--- l10n_eu_oss_oca/migrations/17.0.1.0.0/post-migration.py
+++ l10n_eu_oss_oca/migrations/17.0.1.0.0/post-migration.py
@@ -8,5 +8,8 @@ def migrate(cr, version):
         return
     env = api.Environment(cr, SUPERUSER_ID, {})
     oss_taxes = env["account.tax"].search([("oss_country_id", "!=", False)])
+    oss_tax_groups = oss_taxes.mapped("tax_group_id")
+    for oss_tax_group in oss_tax_groups:
+        oss_tax_group.country_id = oss_tax_group.company_id.account_fiscal_country_id
     for oss_tax in oss_taxes:
         oss_tax.country_id = oss_tax.company_id.account_fiscal_country_id
