cfcf4db2a5b730e90871d0b090597a4e35b4b135
Raphaël Valyi <rvalyi@akretion.com>
2026-05-07 14:41:14 +0000

    [IMP] account_ecotax: pre-commit auto fixes

--- account_ecotax/models/ecotax_line_product.py
+++ account_ecotax/models/ecotax_line_product.py
@@ -22,8 +22,7 @@ class EcotaxLineProduct(models.Model):
     )
     force_amount = fields.Float(
         digits="Ecotax",
-        help="Force ecotax amount.\n"
-        "Allow to substitute default Ecotax Classification",
+        help="Force ecotax amount.\nAllow to substitute default Ecotax Classification",
     )
     amount = fields.Float(
         digits="Ecotax",
